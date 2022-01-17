173ba1ae6aacd3df0f3edd4ebd48028b384118ed
Luiz Felipe Moraes <lffm1994@gmail.com>
2022-01-17 12:46:23 -0300

    Executando programas Java

---
+++ src/com/schoolofnet/HelloWorld.java
@@ -0,0 +1,15 @@
+package com.schoolofnet;
+
+public class HelloWorld {
+	
+	// Padrão CamelCase de nomenclatura de classe
+	// Método de entry point.
+	// Pode ser usado ... ao invés de [] 
+	
+	public static void main(String[] args) {
+		System.out.println("Hello word from School of net.");
+		
+	}
+	
+
+}
